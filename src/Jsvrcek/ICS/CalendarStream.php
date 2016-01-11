4642f4828d150b796aa751edc9538d45a8d4fc5d
Stephan Richter <github@keawe.de>
2016-01-12 00:28:13 +0100

    simplified CalendarStream.addIterm using the php wordwrap function

--- src/Jsvrcek/ICS/CalendarStream.php
+++ src/Jsvrcek/ICS/CalendarStream.php
@@ -37,32 +37,9 @@ class CalendarStream
      * @param string $item
      * @return CalendarStream
      */
-    public function addItem($item)
+    public function addItem($item);
     {
-        //get number of bytes
-        $length = strlen($item);
-        
-        $block = '';
-        
-        if ($length > 75)
-        {
-            $start = 0;
-            
-            while ($start < $length)
-            {
-                $block .= mb_strcut($item, $start, self::LINE_LENGTH, 'UTF-8');
-                $start = $start + self::LINE_LENGTH;
-                
-                //add space if not last line
-                if ($start < $length) $block .= Constants::CRLF.' ';
-            }
-        }
-        else
-        {
-            $block = $item;
-        }
-    
-        $this->stream .= $block.Constants::CRLF;
+        $this->stream .= wordwrap($item,70,Constants::CRLF.' ',true).Constants::CRLF;
         
         return $this;
     }
